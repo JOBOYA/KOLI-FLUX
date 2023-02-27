da5a4114c73b427bc224c1a50ff175ec89f6c8b5
JOSEPH <lena.boya26@gmail.com>
2023-02-27 21:19:21 +0100

    test3

--- src/client/pages/Dashbord/Statistics.tsx
+++ src/client/pages/Dashbord/Statistics.tsx
@@ -31,102 +31,84 @@ const ChartComponent: React.FC = () => {
   const chartRef = useRef<Chart | null>(null);
   const [isSearchButtonClicked, setIsSearchButtonClicked] = useState<boolean>(false);
 
-  const handleSearchButtonClick = React.useCallback(() => {
-    setIsSearchButtonClicked(true);
-  }, [city]);
+  useEffect(() => {
+    axios
+      .request(options) 
+      .then(function (response) {
+        setItems(response.data.result.items);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-  const handleResize = () => {
-    if (canvasRef.current) {
-      const parent = canvasRef.current.parentElement;
-      if (parent) {
-        canvasRef.current.width = parent.clientWidth;
-        canvasRef.current.height = parent.clientHeight;
-      }
-    }
+  useEffect(() => {
     if (chartRef.current) {
-      chartRef.current.resize();
+      chartRef.current.destroy();
     }
-  };
-
-  const fetchItems = async () => {
-    const params = {
-      zipCodes: city,
-      pageIndex: '1',
-      pageSize: '50',
-      realtyTypes: '1',
-      transactionType: '1',
-      sortBy: '0',
-      includeNewConstructions: 'true',
-    };
-    try {
-      const response = await axios.request({ ...options, params });
-      const items = response.data.items.map((item: any) => ({
-        id: item.id,
-        Object: {
-          city: item.city,
-          price: item.price,
+    const ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
+      const chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: items.map((item) => item.Object.city),
+          datasets: [
+            {
+              label: 'Prix',
+              data: items.map((item) => item.Object.price),
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+            },
+          ],
         },
-      }));
-      setItems(items);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-  useEffect(() => {
-    fetchItems();
-  }, [city]);
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d');
-      if (ctx) {
-        new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: items.map((item) => item.Object.city),
-            datasets: [
+        options: {
+          scales: {
+            yAxes: [
               {
-                label: 'Prix',
-                data: items.map((item) => item.Object.price),
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1,
+                ticks: {
+                  beginAtZero: true,
+                },
               },
             ],
           },
-          options: {
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true,
-                  },
-                },
-              ],
-            },
-          },
-        });
-      }
+        },
+      });
+      chartRef.current = chart;
     }
-
-    
-    return () => {
-      if (!isSearchButtonClicked) {
-        setItems([]);
-      }
-    };
   }, [items]);
   
-  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCity(event.target.value);
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
   };
+
+  const handleSearchButtonClick = () => {
+    setIsSearchButtonClicked(true);
+  };
+
+  useEffect(() => {
+    if (isSearchButtonClicked) {
+      const newOptions = {
+        ...options,
+        params: {
+          ...options.params,
+          zipCodes: city,
+        },
+      };
+
+      axios
+        .request(newOptions)
+        .then(function (response) {
+          setItems(response.data.result.items);
+        })
+        .catch(function (error) { 
+          console.error(error);
+        });
+    }
+  }, [isSearchButtonClicked]);
+
+
+
 
   return (
     <div>
